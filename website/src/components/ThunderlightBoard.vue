40e8546c284673b3d29757021508fa30a06e3854
Anatoly Frolov <anatolyfroloff@gmail.com>
2024-03-10 15:40:15 +1000

    Create `ThunderlightBoard.vue`

---
+++ website/src/components/ThunderlightBoard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div id="board">
+        <div class="cell" v-for="_ in 9 ** 2">
+        </div>
+    </div>
+</template> 
+
+<style scoped lang="sass">
+$cell-size: 4em
+$cell-gap: 0.66em
+
+#board
+    display: grid
+    aspect-ratio: 1 / 1
+    grid-template-rows: repeat(9, $cell-size)
+    grid-template-columns: repeat(9, $cell-size)
+    gap: $cell-gap
+
+.cell
+    background: $gray
+    display: flex
+    align-items: center
+    justify-content: center
+
+ion-icon
+    color: $primary
+</style>
