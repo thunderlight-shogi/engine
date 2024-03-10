466429f5949be0e1fd79f8b6b9e9af2619b483ee
Anatoly Frolov <anatolyfroloff@gmail.com>
2024-03-10 15:39:36 +1000

    Add global Sass styles to Vite

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    resolve: {
+        alias: {
+          '@': path.resolve(__dirname, 'src'),
+        }
+      },
+    css: {
+        preprocessorOptions: {
+            sass: {
+                additionalData: `@import "@/assets/global.sass"`
+            }
+        }
+    }
 })
