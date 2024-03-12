6acc0bbf2b5359c5a7852158b1060f80dbee9c24
Anatoly Frolov <anatolyfroloff@gmail.com>
2024-03-12 13:30:16 +1000

    Add default layout (sketch)

--- website/src/components/Board.vue
+++ website/src/components/Board.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { byClass, closest, heightOf, widthOf } from '../dom/dom';
+import { byClass, closest, heightOf, locate, locateMouse, measure, move, widthOf } from '../dom/dom';
+import { firecracker } from '../particles/particles';
 import { pick } from '../utils/arrays';
 import { flipCoin } from '../utils/booleans';
-import { Location2D } from '../utils/geometry';
+import { Location2D, distanceBetween } from '../utils/geometry';
 import { jukebox } from '../utils/jukebox';
+import { translate } from '../utils/numbers';
+import { sleep } from '../utils/sleep';
 import Piece from './Piece.vue';
 
 
@@ -14,7 +17,9 @@ function getCells(): HTMLElement[] {
 }
 
 function fadeCells() {
-    getCells().forEach(cell => cell.classList.remove('highlighted'));
+    getCells().forEach(cell => {
+        cell.classList.remove('highlighted')
+    });
 }
 
 function highlight(cell: HTMLElement) {
@@ -26,11 +31,12 @@ function onPieceGrab(element: HTMLElement) {
     hand = element;
 }
 
-function onPieceDrop(_: HTMLElement) {
+async function onPieceDrop(_: HTMLElement, mouseLocation: Location2D) {
     if (hand === undefined) {
         return;
     }
 
+    const location = locate(hand);
     const cells = getCells();
     const underlyingCell = closest(cells, hand);
 
@@ -45,7 +51,13 @@ function onPieceDrop(_: HTMLElement) {
         jukebox.play("piece.back", 0.3);
     } else {
         underlyingCell.replaceChildren(hand);
-        jukebox.play("piece.attack", 0.5);
+        hand = undefined;
+        
+        await sleep(100);
+        jukebox.play("piece.attack", 0.5, 1);
+
+        await sleep(20);
+        firecracker.splash("piece.shred", locate(underlyingCell), 20, 25);
     }
     
     fadeCells();
@@ -57,11 +69,7 @@ function onPieceMove(event: MouseEvent): void {
         return;
     }
 
-    const mouse = new Location2D(event.pageX + widthOf(hand) / 4, event.pageY + heightOf(hand) / 4);
-    
-    hand.style.left = `${mouse.x}px`;
-    hand.style.top = `${mouse.y}px`;
-
+    const mouse = locateMouse(event).shift(measure(hand).shorten(3));
     const cells = getCells();
     const underlyingCell = closest(cells, hand);
 
@@ -69,18 +77,62 @@ function onPieceMove(event: MouseEvent): void {
         fadeCells();
     }
 
+    move(hand, mouse);
     highlight(underlyingCell);
 }
+
+class PieceType {
+    constructor(public readonly kanji: string, public readonly promoted: boolean = false) {}
+}
+
+class ActivePiece {
+    constructor(public readonly type: PieceType,
+                public readonly mine: boolean) {}
+}
+
+const pawn: PieceType = { kanji: '歩', promoted: false };
+const king: PieceType = { kanji: '王', promoted: false };
+const rook: PieceType = { kanji: '飛', promoted: false };
+const dragon: PieceType = { kanji: '龍', promoted: false };
+const bishop: PieceType = { kanji: '角', promoted: false };
+const horse: PieceType = { kanji: '馬', promoted: false };
+const gold: PieceType = { kanji: '金', promoted: false };
+const silver: PieceType = { kanji: '銀', promoted: false };
+const knight: PieceType = { kanji: '桂', promoted: false };
+const goldenKnight: PieceType = { kanji: '今', promoted: false };
+const lance: PieceType = { kanji: '香', promoted: false };
+const goldenLance: PieceType = { kanji: '杏', promoted: false };
+const tokin: PieceType = { kanji: 'と', promoted: false };
+
+function e(type: PieceType) {
+    return new ActivePiece(type, false);
+}
+
+function f(type: PieceType) {
+    return new ActivePiece(type, true);
+}
+
+const board: (ActivePiece | null)[] = [
+    e(lance), e(knight), e(silver), e(gold), e(king), e(gold), e(silver), e(knight), e(lance),
+    null, e(rook), null, null, null, null, null, e(bishop), null,
+    e(pawn), e(pawn), e(pawn), e(pawn), e(pawn), e(pawn), e(pawn), e(pawn), e(pawn),
+    null, null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null, null, null,
+    f(pawn), f(pawn), f(pawn), f(pawn), f(pawn), f(pawn), f(pawn), f(pawn), f(pawn),
+    null, f(bishop), null, null, null, null, null, f(rook), null,
+    f(lance), f(knight), f(silver), f(gold), f(king), f(gold), f(silver), f(knight), f(lance),
+];
 </script>
 
 <template>
     <div id="board" @mousemove="onPieceMove">
-        <div class="cell" v-for="_ in 9 ** 2">
+        <div class="cell" v-for="piece of board">
             <Piece 
-                :kanji="pick(['王', '飛', '龍', '角', '馬', '金', '銀', '全', '桂', '今', '香', '杏', '歩', 'と'])" 
-                :promoted="flipCoin(0.1)" 
-                :grabbable="flipCoin(0.3)"
-                v-if="flipCoin(0.2)"
+                :kanji="piece?.type.kanji"
+                :promoted="piece?.type.promoted" 
+                :grabbable="piece?.mine"
+                v-if="piece !== null"
                 @grab="onPieceGrab"
                 @drop="onPieceDrop"
             ></Piece>
@@ -116,7 +168,7 @@ $cell-gap: 0.66em
     &.highlighted
         background: $primary
         border: 1px cyan solid
-        box-shadow: 0 0 5px $primary
+        box-shadow: 0 0 30px $primary
 
 ion-icon
     color: $primary
