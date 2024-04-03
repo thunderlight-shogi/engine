e8444a4026765fe335a5fd78697a7883b8b8ac9a
Darkmun73 <mrgame7337@gmail.com>
2024-04-03 17:03:44 +1000

    Restructured board and movegen + added some checks
    Added check for move that leads piece to no moves.
    So now possible states don't contain states, e. g., with pawn on last rank.
    
    Fixed bug: dropped piece belonged to opponent.
    
    Now inventory doesn't contain Piece objects, but PieceType objects

---
+++ internal/engine/board/inventory.go
@@ -0,0 +1,57 @@
+package board
+
+import (
+	"slices"
+
+	"github.com/thunderlight-shogi/engine/internal/model"
+)
+
+type inventory struct {
+	pieces []*model.PieceType
+}
+
+type Inventory = *inventory
+
+func newInventory() Inventory {
+	return &inventory{pieces: make([]*model.PieceType, 0)}
+}
+
+func (this_inv Inventory) clone() (inv Inventory) {
+	inv = new(inventory)
+	inv.pieces = make([]*model.PieceType, len(this_inv.pieces))
+	copy(inv.pieces, this_inv.pieces)
+	return
+}
+
+func (this_inv Inventory) Pieces() []*model.PieceType {
+	if len(this_inv.pieces) == 0 {
+		return nil
+	}
+	return this_inv.pieces
+}
+
+func (this_inv Inventory) AddPiece(piece *Piece) {
+	var addedPiece *model.PieceType
+	if piece.IsPromoted() {
+		addedPiece = piece.Type.DemotePiece
+	} else {
+		addedPiece = &piece.Type
+	}
+	this_inv.pieces = append(this_inv.pieces, addedPiece)
+}
+
+// deleting element with index i from array
+func (this_inv Inventory) removePiece(i int) {
+	this_inv.pieces[i] = this_inv.pieces[len(this_inv.pieces)-1]
+	this_inv.pieces = this_inv.pieces[:len(this_inv.pieces)-1]
+}
+
+// maybe replace *pieceType with pieceType name
+func (this_inv Inventory) ExtractPieceToPlayer(pieceType *model.PieceType, player model.Player) *Piece {
+	idx := slices.Index(this_inv.pieces, pieceType)
+	if idx != -1 {
+		this_inv.removePiece(idx)
+		return &Piece{Type: *pieceType, Player: player}
+	}
+	return nil
+}
--- internal/engine/movegen/movegen.go
+++ internal/engine/movegen/movegen.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/thunderlight-shogi/engine/internal/engine/board"
 	"github.com/thunderlight-shogi/engine/internal/model"
-	"github.com/thunderlight-shogi/engine/pkg/graphics"
 )
 
 //TODO: Везде поменять vertical и horizontal на file и rank
@@ -18,14 +17,6 @@ type GameState struct {
 	NumOfDropableCells        uint //TODO: Сделать подсчет
 }
 
-func (gs *GameState) getShiftSign() int {
-	if gs.CurMovePlayer == model.Sente {
-		return 1
-	} else {
-		return -1
-	}
-}
-
 func (gs *GameState) getNextPlayer() model.Player {
 	if gs.CurMovePlayer == model.Sente {
 		return model.Gote
@@ -34,92 +25,14 @@ func (gs *GameState) getNextPlayer() model.Player {
 	}
 }
 
-func (gs *GameState) getPromotionZone() []int {
-	if gs.CurMovePlayer == model.Sente {
-		return board.PromotionZoneForSente
-	} else {
-		return board.PromotionZoneForGote
-	}
-}
-
-func (gs *GameState) canPieceReachCell(vPieceCoord int, hPieceCoord int, vCellCoord int, hCellCoord int) bool {
-	var isReached bool = true
-
-	var curBoard = gs.Board
-	var piece = curBoard.Cells[vPieceCoord][hPieceCoord]
-	var moves = piece.Type.Moves
-	var shiftSign = gs.getShiftSign()
-
-	origin := graphics.NewPoint(vPieceCoord, hPieceCoord)
-	end := graphics.NewPoint(vCellCoord, hCellCoord)
-	coordsBetween := graphics.GetLinePoints(origin, end)
-	for i := 1; i < len(coordsBetween)-1; i++ { // skipping origin and end
-		vMiddleCoord, hMiddleCoord := coordsBetween[i].Coordinates()
-		hShift, vShift := vMiddleCoord-vPieceCoord, hMiddleCoord-hPieceCoord
-		// maybe better to replace with simple loop
-		idx := slices.IndexFunc(moves, func(move model.Move) bool {
-			return move.HorizontalShift*shiftSign == hShift && move.VerticalShift*shiftSign == vShift
-		})
-		if idx != -1 { // if move between origin and end was found in moves of piece
-			emptyCell := curBoard.Cells[vMiddleCoord][hMiddleCoord] == nil
-			if !emptyCell {
-				isReached = false
-				break
-			}
-		}
-	}
-	return isReached
-}
-
-func (gs *GameState) getPossibleMoves(verticalCoord int, horizontalCoord int) (movesCoords [][2]int) {
-	movesCoords = [][2]int{}
-
-	var curBoard = gs.Board
-	var shiftSign = gs.getShiftSign()
-	var piece = curBoard.Cells[verticalCoord][horizontalCoord]
-	var moves = piece.Type.Moves
-	for _, move := range moves {
-		var vMoveCoord = verticalCoord + move.HorizontalShift*shiftSign
-		var hMoveCoord = horizontalCoord + move.VerticalShift*shiftSign
-
-		var inBoardField bool = vMoveCoord >= 0 && vMoveCoord < len(curBoard.Cells) && hMoveCoord >= 0 && hMoveCoord < len(curBoard.Cells[vMoveCoord])
-		if inBoardField {
-			var emptyOrEnemyCell bool = curBoard.Cells[vMoveCoord][hMoveCoord] == nil || curBoard.Cells[vMoveCoord][hMoveCoord].Player != gs.CurMovePlayer
-			if emptyOrEnemyCell {
-				if gs.canPieceReachCell(verticalCoord, horizontalCoord, vMoveCoord, hMoveCoord) {
-					movesCoords = append(movesCoords, [2]int{vMoveCoord, hMoveCoord})
-				}
-			}
-		}
-	}
-	return
-}
-
-// TODO: Добавить PieceType в параметры (У разных фигур могут быть разные клетки сброса)
-// TODO: Учитывать, сможет ли фигура пойти дальше (при ходе тоже)
-func (gs *GameState) getPossibleDrops() (dropsCoords [][2]int) {
-	dropsCoords = [][2]int{}
-
-	var curBoard = gs.Board
-	for vDropCoord, verticalCells := range curBoard.Cells {
-		for hDropCoord, cell := range verticalCells {
-			if cell == nil { // empty cell
-				dropsCoords = append(dropsCoords, [2]int{vDropCoord, hDropCoord})
-			}
-		}
-	}
-	return
-}
-
 func (gs *GameState) getPossibleStatesFromBoardPiece(verticalCoord int, horizontalCoord int) (gss []GameState) {
 	gss = []GameState{}
 
 	var curBoard = gs.Board
 	var nextPlayer = gs.getNextPlayer()
-	var movesCoords [][2]int = gs.getPossibleMoves(verticalCoord, horizontalCoord)
+	var movesCoords [][2]int = curBoard.GetPossibleMoves(verticalCoord, horizontalCoord)
 	for _, coords := range movesCoords {
 		var newBoard = curBoard.Clone()
-		//создавать gamestate прям тут и позже из него вызывать какой-нибудь метод?
 		var cellMoveTo = newBoard.Cells[coords[0]][coords[1]]
 
 		var emptyCell = cellMoveTo == nil
@@ -128,9 +41,10 @@ func (gs *GameState) getPossibleStatesFromBoardPiece(verticalCoord int, horizont
 		}
 
 		// creating alternative gamestate for promotion of piece
-		if newBoard.Cells[verticalCoord][horizontalCoord].IsPromotable() {
-			var inPromotionZone = slices.Contains(gs.getPromotionZone(), coords[1])
-			var fromPromotionZone = slices.Contains(gs.getPromotionZone(), horizontalCoord)
+		var boardPiece = newBoard.Cells[verticalCoord][horizontalCoord]
+		if boardPiece.IsPromotable() {
+			var inPromotionZone = slices.Contains(curBoard.GetPromotionZone(boardPiece.Player), coords[1])
+			var fromPromotionZone = slices.Contains(curBoard.GetPromotionZone(boardPiece.Player), horizontalCoord)
 			if inPromotionZone || fromPromotionZone {
 				altNewBoard := newBoard.Clone()
 				altNewBoard.Cells[coords[0]][coords[1]] = &board.Piece{Type: *altNewBoard.Cells[verticalCoord][horizontalCoord].Type.PromotePiece, Player: gs.CurMovePlayer}
@@ -144,8 +58,11 @@ func (gs *GameState) getPossibleStatesFromBoardPiece(verticalCoord int, horizont
 		newBoard.Cells[coords[0]][coords[1]] = newBoard.Cells[verticalCoord][horizontalCoord]
 		newBoard.Cells[verticalCoord][horizontalCoord] = nil
 
-		newGameState := GameState{Board: newBoard, CurMovePlayer: nextPlayer}
-		gss = append(gss, newGameState)
+		// e. g., pawn cannot move to last row because it will have no moves to board field (so this is not possible game state)
+		if len(newBoard.GetInBoardFieldMoves(coords[0], coords[1])) != 0 {
+			newGameState := GameState{Board: newBoard, CurMovePlayer: nextPlayer}
+			gss = append(gss, newGameState)
+		}
 	}
 	return
 }
@@ -172,22 +89,27 @@ func (gs *GameState) getPossibleStatesFromInventoryPieces() (gss []GameState) {
 	gss = []GameState{}
 
 	var curBoard = gs.Board
+	var curPlayer = gs.CurMovePlayer
 	var nextPlayer = gs.getNextPlayer()
-	for _, piece := range curBoard.Inventories[gs.CurMovePlayer].Pieces() {
-		dropsCoords := gs.getPossibleDrops()
+
+	for _, pieceType := range curBoard.Inventories[gs.CurMovePlayer].Pieces() {
+		var dropsCoords = curBoard.GetPossibleDrops()
 		for _, coords := range dropsCoords {
 			newBoard := curBoard.Clone()
-			newBoard.Cells[coords[0]][coords[1]] = newBoard.Inventories[gs.CurMovePlayer].ExtractPiece(piece)
+			newBoard.Cells[coords[0]][coords[1]] = newBoard.Inventories[curPlayer].ExtractPieceToPlayer(pieceType, curPlayer)
 
-			newGameState := GameState{Board: newBoard, CurMovePlayer: nextPlayer}
-			gss = append(gss, newGameState)
+			// e. g., pawn cannot move to last row because it will have no moves to board field (so this is not possible game state)
+			if len(newBoard.GetInBoardFieldMoves(coords[0], coords[1])) != 0 {
+				newGameState := GameState{Board: newBoard, CurMovePlayer: nextPlayer}
+				gss = append(gss, newGameState)
+			}
 		}
 	}
 	return
 }
 
 func (gs *GameState) GetPossibleStates() (gss []GameState) {
-	gss = make([]GameState, 0)
+	gss = []GameState{}
 
 	gss = append(gss, gs.getPossibleStatesFromBoardPieces()...)
 	gss = append(gss, gs.getPossibleStatesFromInventoryPieces()...)
