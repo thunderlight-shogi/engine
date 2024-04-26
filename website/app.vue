a07e14ecbbc23790da017dd63bf0c9ea6bb4eab3
Anatoly Frolov <anatolyfroloff@gmail.com>
2024-04-26 20:39:20 +1000

    Move the app component from the old website

--- website/app.vue
+++ website/app.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+import Board from './components/Board.vue';
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Board></Board>
 </template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
